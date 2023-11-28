0d8159182307994385d9af1aae5823a0e4e3c46a
LucasGomeszx <lgomeszx@gmail.com>
2023-11-27 21:25:01 -0300

    Criando projeto.

---
+++ MyNFT/MyNFT/MyNFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNFTApp.swift
+//  MyNFT
+//
+//  Created by Lucas Gomesx on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MyNFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
