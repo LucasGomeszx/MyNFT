b6c1e7c4fd6a0cda5e9ba109cd3450238c54bff1
LucasGomeszx <lgomeszx@gmail.com>
2023-11-28 19:08:05 -0300

    Organizando view em blocos

--- MyNFT/MyNFT/Register/View/RegisterView.swift
+++ MyNFT/MyNFT/Register/View/RegisterView.swift
@@ -13,45 +13,16 @@ struct RegisterView: View {
     
     var body: some View {
         ZStack {
+
             Color.backgroundColor.ignoresSafeArea()
             
             VStack(spacing: 25) {
-                Text("Register")
-                    .font(.system(size: 55, weight: .bold))
-                    .foregroundStyle(.white)
-                    .padding(.top, 20)
-                    .padding(.bottom, 50)
                 
-                Group {
-                    TextField("", text: $viewModel.email, prompt: Text("E-mail").foregroundStyle(Color.placeholderColor))
-                        .keyboardType(.emailAddress)
-                    SecureField("", text: $viewModel.password, prompt: Text("Password").foregroundStyle(Color.placeholderColor))
-                    SecureField("", text: $viewModel.confirmPassword, prompt: Text("Repita a senha").foregroundStyle(Color.placeholderColor))
-                }
-                .frame(height: 40)
-                .padding(7)
-                .overlay(content: {
-                    RoundedRectangle(cornerRadius: 8).stroke(Color.fieldBorderColor, lineWidth: 2)
-                })
-                .padding(7)
-                .foregroundStyle(.white)
-                .autocorrectionDisabled()
-                .textInputAutocapitalization(.never)
+                HeadView()
                 
                 Spacer()
                 
-                Button {
-                    
-                } label: {
-                    Text("Cadastrar")
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 45)
-                        .foregroundStyle(.white)
-                        .font(.system(size: 18, weight: .bold))
-                        .background(!viewModel.isDisabledRegisterButton ? Color.buttonColor : Color.buttonColor.opacity(0.6))
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                }
-                .disabled(viewModel.isDisabledRegisterButton)
+                RegisterButton()
                 
             }
             .padding(.horizontal, 20)
@@ -67,9 +38,66 @@ struct RegisterView: View {
         .navigationDestination(isPresented: $viewModel.goHome) {
             
         }
+        .environmentObject(viewModel)
         
     }
     
+}
+
+//MARK: - HeadView
+
+struct HeadView: View {
+    
+    @EnvironmentObject var viewModel: RegisterViewModel
+    
+    var body: some View {
+
+        
+        Text("Register")
+            .font(.system(size: 55, weight: .bold))
+            .foregroundStyle(.white)
+            .padding(.top, 20)
+            .padding(.bottom, 50)
+        
+        Group {
+            TextField("", text: $viewModel.email, prompt: Text("E-mail").foregroundStyle(Color.placeholderColor))
+                .keyboardType(.emailAddress)
+            SecureField("", text: $viewModel.password, prompt: Text("Password").foregroundStyle(Color.placeholderColor))
+            SecureField("", text: $viewModel.confirmPassword, prompt: Text("Repita a senha").foregroundStyle(Color.placeholderColor))
+        }
+        .frame(height: 40)
+        .padding(7)
+        .overlay(content: {
+            RoundedRectangle(cornerRadius: 8).stroke(Color.fieldBorderColor, lineWidth: 2)
+        })
+        .padding(7)
+        .foregroundStyle(.white)
+        .autocorrectionDisabled()
+        .textInputAutocapitalization(.never)
+    }
+    
+}
+
+//MARK: - RegisterButton
+
+struct RegisterButton: View {
+    
+    @EnvironmentObject var viewModel: RegisterViewModel
+    
+    var body: some View {
+        Button {
+            
+        } label: {
+            Text("Cadastrar")
+                .frame(maxWidth: .infinity)
+                .frame(height: 45)
+                .foregroundStyle(.white)
+                .font(.system(size: 18, weight: .bold))
+                .background(!viewModel.isDisabledRegisterButton ? Color.buttonColor : Color.buttonColor.opacity(0.6))
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+        }
+        .disabled(viewModel.isDisabledRegisterButton)
+    }
 }
 
 #Preview {
